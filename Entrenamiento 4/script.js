8087255e679af6c5c5999bbc62b6cbd0fc807837
147719366+VictorG91@users.noreply.github.com <147719366+VictorG91@users.noreply.github.com>
2024-09-30 22:51:05 +0200

    no message

--- Entrenamiento 4/script.js
+++ Entrenamiento 4/script.js
@@ -1,21 +1,45 @@
-document.addEventListener('DOMContentLoaded', function(event){
-
+document.addEventListener('DOMContentLoaded', function (event) {
     const btnSubmit = document.getElementById('submit');
-    var nombre = document.getElementById('nombre');
-    var edad = document.getElementById('edad');
-    var email = document.getElementById('email');
-    var telephone = document.getElementById('phone');
-
-    btnSubmit.addEventListener('click', function(event){
-        
-        
-
-    })
     
-})
+    btnSubmit.addEventListener('click', function(event) {
 
-function validarEmail(email) {
-    // Expresión regular para validar un correo electrónico
-    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    return regex.test(email);
-}
+    
+        const nombre = document.getElementById('nombre').value;
+        const edad = Number(document.getElementById('edad').value);
+        const email = document.getElementById('email').value;
+        const telefono = document.getElementById('telefono').value;
+    
+    
+        if (!validarEmail(email)) {
+            alert('Introduzca un email valido.');
+            return;
+        }
+    
+        if (nombre.length < 3) {
+            alert('El nombre debe de tener al menos 3 caracteres.');
+            return;
+        }
+    
+        if (!validarTelefono(telefono)) {
+            alert('Introduzca un numero de telefono con 9 digitos.');
+            return;
+        }
+    
+        if (edad < 0 || edad > 120) {
+            alert('Introduzca una edad correcta entre 0 y 120.');
+            return;
+        }
+    });
+    
+    });
+    
+    function validarEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    
+     
+    function validarTelefono(telefono){
+        const telefonoRegex = /^[0-9]{9}$/;
+        return telefonoRegex.test(telefono);
+    }
