aef3f90ecc8396eeb336ec70e82732b95033f191
COMPU-S035\daw2 <vgarciag11@educantabria.es>
2024-09-30 16:21:24 +0200

    rama principañ

---
+++ Entrenamiento 4/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(event){
+
+    const btnSubmit = document.getElementById('submit');
+    var nombre = document.getElementById('nombre');
+    var edad = document.getElementById('edad');
+    var email = document.getElementById('email');
+    var telephone = document.getElementById('phone');
+
+    btnSubmit.addEventListener('click', function(event){
+        validarEmail()
+
+
+    })
+    
+})
+
+function validarEmail(email) {
+    // Expresión regular para validar un correo electrónico
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+}
